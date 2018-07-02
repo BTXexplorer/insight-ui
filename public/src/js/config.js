0ae588820fb5c1cb3a43a5ac7ff6eb051a68466e
Tadeáš Kmenta <TheTrunk@users.noreply.github.com>
2018-07-02 13:15:19 +0200

    BTX

--- public/src/js/config.js
+++ public/src/js/config.js
@@ -4,7 +4,7 @@ angular.module('insight')
 .constant('Constants', {
 
 	CURRENCY: {
-		RVN : 'RVN',
+		BTX : 'BTX',
 		USD : 'USD',
 		BTC : 'BTC'
 	},
@@ -14,7 +14,7 @@ angular.module('insight')
 	CHART_DAYS: 14,
     NETWORK: window.current_network ? window.current_network : 'livenet',
     DEFAULT_LANGUAGE: localStorage.getItem('insight-language') || 'en',
-    DEFAULT_CURRENCY: localStorage.getItem('insight-currency') || 'RVN'
+    DEFAULT_CURRENCY: localStorage.getItem('insight-currency') || 'BTX'
 
 });
 //Setting up route
@@ -22,7 +22,7 @@ angular.module('insight').config(function($routeProvider) {
   $routeProvider.
     when('/block/:blockHash', {
       templateUrl: 'views/block.html',
-      title: 'Ravencoin Block '
+      title: 'bitcore Block '
     }).
     when('/block-index/:blockHeight', {
       controller: 'BlocksController',
@@ -34,7 +34,7 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/tx/:txId/:v_type?/:v_index?', {
       templateUrl: 'views/transaction.html',
-      title: 'Ravencoin Transaction '
+      title: 'bitcore Transaction '
     }).
     when('/', {
       templateUrl: 'views/index.html',
@@ -42,15 +42,15 @@ angular.module('insight').config(function($routeProvider) {
     }).
     when('/blocks', {
       templateUrl: 'views/block_list.html',
-      title: 'Ravencoin Blocks solved Today'
+      title: 'bitcore Blocks solved Today'
     }).
     when('/blocks-date/:blockDate/:startTimestamp?', {
       templateUrl: 'views/block_list.html',
-      title: 'Ravencoin Blocks solved '
+      title: 'bitcore Blocks solved '
     }).
     when('/address/:addrStr', {
       templateUrl: 'views/address.html',
-      title: 'Ravencoin Address '
+      title: 'bitcore Address '
     }).
 	 when('/charts', {
       templateUrl: 'views/charts.html',
